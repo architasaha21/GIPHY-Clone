a696d2a08ff2387dd6b39fa4baae3f9c1de23345
architasaha21 <archita.saha2106@gmail.com>
2025-06-22 01:33:18 +0530

    Updates

---
+++ src/components/gif.jsx
@@ -0,0 +1,9 @@
+
+const gif = ({gif, hover=true}) => {
+  return <Link to={`/${gif.type}/${gif.slug}`} className="block mb-2">
+  
+  
+  </Link>
+};
+
+export default gif;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import {useEffect} from "react";
+import {GifState} from "../context/gif-context";
+import Gif from "../components/gif";
+import FilterGif from "../components/filter-gif";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  const {gf, gifs, setGifs} = GifState();
+
+  const fetchTrendingGIFs = async () => {
+    const {data} = await gf.trending({
+    limit: 20,
+    type: filter,
+    rating: "g",
+    });
+    setGifs(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingGIFs();
+  }, [filter]);
+
+  return (
+    <div>
+       <img
+        src="/banner.gif"
+        alt="earth banner"
+        className="mt-2 rounded w-full"
+      />
+
+      <FilterGif />
+
+      <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
+        {gifs.map((gif) => (
+          <Gif gif={gif} key={gif.id} />
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
